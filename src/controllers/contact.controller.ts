b33aa753f8ee60008e8eff2a41bf33d15efbf535
T Lidanski <t_lidanski@abv.bg>
2022-08-11 16:16:17 +0300

    Getting contacts with pagination and deleting a given contact

--- src/controllers/contact.controller.ts
+++ src/controllers/contact.controller.ts
@@ -16,6 +16,8 @@ export class ContactController implements Controller {
 
     public initRoutes = (): void => {
         this.router.post(`${this.path}`, this.jwtService.validateToken, this.create);
+        this.router.get(`${this.path}/:skip/:take`, this.jwtService.validateToken, this.getAll);
+        this.router.delete(`${this.path}/:id`, this.jwtService.validateToken, this.delete);
     }
 
     private create = async (req: Request, res: Response): Promise<Response | void> => {
@@ -23,6 +25,30 @@ export class ContactController implements Controller {
             const contact = this.contactRepository.create(req.body);
             const result = await this.contactRepository.save(contact);
             return res.status(201).json(result);
+        } catch (error) {
+            return res.status(400).json(error);
+        }
+    }
+
+    private getAll = async (req: Request, res: Response): Promise<Response | void> => {
+        try {
+            const {skip, take} = req.params;
+            const contacts = await this.contactRepository.find({
+                relations: ['user'],
+                skip: Number(skip),
+                take: Number(take)
+            });
+
+            return res.status(200).json(contacts);
+        } catch (error) {
+            return res.status(400).json(error);
+        }
+    }
+
+    private delete = async (req: Request, res: Response): Promise<Response | void> => {
+        try {
+            const result = await this.contactRepository.delete(req.params.id);
+            return res.status(202).json(result);
         } catch (error) {
             return res.status(400).json(error);
         }
