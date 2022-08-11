2f3ae217dc86051c674409a84d2f818dc76931ff
T Lidanski <t_lidanski@abv.bg>
2022-08-11 17:05:56 +0300

    contact controller init

--- src/app-init.ts
+++ src/app-init.ts
@@ -3,11 +3,13 @@ import App from './app';
 import 'dotenv/config';
 import express from 'express';
 import { AuthController } from './controllers/auth.controller';
+import { ContactController } from './controllers/contact.controller';
 
 export const app: App = new App({
     port: Number(process.env.PORT),
     controllers: [
-        new AuthController()
+        new AuthController(),
+        new ContactController()
     ],
     middlewares: [
         express.json()
